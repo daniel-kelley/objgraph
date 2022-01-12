aaa76cd848a3fbaf1c485bd57788c0990364f2e9
Daniel Kelley <dkelley@gmp.san-jose.ca.us>
2022-01-12 07:15:11 -0800

    Initial outline

---
+++ lib/objgraph.rb
@@ -0,0 +1,18 @@
+#
+#  objgraph.rb
+#
+#  Copyright (c) 2022 by Daniel Kelley
+#
+# SPDX-License-Identifier: MIT
+#
+
+class ObjGraph
+  def initialize(options)
+  end
+
+  def scan
+  end
+
+  def report
+  end
+end
